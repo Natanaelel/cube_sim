29cc14e85349c737ffd1cf83b3197c4a03eed56b
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2021-12-04 22:18:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,316 @@
+let scene
+let camera
+let renderer
+let mouse = {x: 0, y: 0}
+let controls
+let material
+let ambientLight
+let directionalLight
+
+
+let clock = new THREE.Clock();
+
+
+let theCube
+let animationTime = 20
+
+let rotateDict = {
+  "right": {
+    "edges": [[1, 0], [6, 0], [9, 0], [4, 0]],
+    "corners": [[1, 1], [6, 2], [5 ,1], [2, 2]]
+  },
+  "left": {
+    "edges": [[3, 0], [5, 0], [11, 0], [7, 0]],
+    "corners": [[0, 2], [3, 1], [4 ,2], [7, 1]]
+  },
+  "up": {
+    "edges": [[0, 0], [1, 0], [2, 0], [3, 0]],
+    "corners": [[0, 0], [1, 0], [2 ,0], [3, 0]]
+  },
+  "down": {
+    "edges": [[8, 0], [9, 0], [10, 0], [11, 0]],
+    "corners": [[4, 0], [5, 0], [6 ,0], [7, 0]]
+  },
+  "front": {
+    "edges": [[2, 1], [4, 1], [8, 1], [5, 1]],
+    "corners": [[3, 2], [2, 1], [5 ,2], [4, 1]]
+  },
+  "back": {
+    "edges": [[0, 1], [7, 1], [10, 1], [6, 1]],
+    "corners": [[1, 2], [0, 1], [7 ,2], [6, 1]]
+  }
+}
+edgeRotationOffsets = [
+  ["up", "back"],
+  ["up", "right"],
+  ["up", "front"],
+  ["up", "left"],
+  ["front", "right"],
+  ["front", "left"],
+  ["back", "right"],
+  ["back", "left"],
+  ["down", "front"],
+  ["down", "right"],
+  ["down", "back"],
+  ["down", "left"]
+]
+cornerRotationOffsets = [
+  ["up", "left", "back"],
+  ["up", "back", "right"],
+  ["up", "right", "front"],
+  ["up", "front", "left"],
+  ["down", "left", "front"],
+  ["down", "front", "right"],
+  ["down", "right", "back"],
+  ["down", "back", "left"]
+]
+centerRotationOffsets = [
+  "up",
+  "front",
+  "right",
+  "back",
+  "left",
+  "down"
+]
+let rotation = {
+  "right": {
+    angle: 0,
+    dir: new THREE.Vector3(1, 0, 0)
+  },
+  "left": {
+    angle: 0,
+    dir: new THREE.Vector3(-1, 0, 0)
+  },
+  "up": {
+    angle: 0,
+    dir: new THREE.Vector3(0, 1, 0)
+  },
+  "down": {
+    angle: 0,
+    dir: new THREE.Vector3(0, -1, 0)
+  },
+  "front": {
+    angle: 0,
+    dir: new THREE.Vector3(0, 0, 1)
+  },
+  "back": {
+    angle: 0,
+    dir: new THREE.Vector3(0, 0, -1)
+  }
+}
+
+class Cube{
+  constructor(){
+    this.edges
+    this.corners
+    this.resetCube()
+  }
+  resetCube(){
+    let edges = []
+    let corners = []
+    for(let i=0;i<12;i++){
+      edges.push([i,0])
+    }
+    for(let i=0;i<8;i++){
+      corners.push([i,0])
+    }
+    this.edges = edges
+    this.corners = corners
+    resetScene()
+    return({edges: edges, corners: corners})
+  }
+  rotate(side = "right", turn = 1){
+    if(rotateDict[side]){
+      
+      animateRotation(side, turn * Math.PI * 0.5 - Math.PI, animationTime)
+      
+      let matrix = rotateDict[side]
+      let newEdges = matrix.edges.map(index=>this.edges[index[0]])
+
+      let newCorners = matrix.corners.map(index=>this.corners[index[0]])
+
+      for(let i = 0; i < turn; i++){
+        newEdges = [newEdges[newEdges.length - 1], ...newEdges.slice(0, newEdges.length - 1)].map((p,i)=>[p[0], (p[1]+matrix.edges[i][1])%2]) // edges
+
+        newCorners = [newCorners[newCorners.length - 1], ...newCorners.slice(0, newCorners.length - 1)].map((p,i)=>[p[0], (p[1]+matrix.corners[i][1])%3]) // corners
+      }
+
+      matrix.edges.map((x,i)=>this.edges[x[0]] = newEdges[i])
+      matrix.corners.map((x,i)=>this.corners[x[0]] = newCorners[i])
+
+    }
+    resetScene()
+  }
+  applyMoves(algoritm){
+    for(let move of algoritm.split(" ")){
+      let side = {
+        "U": "up",
+        "D": "down",
+        "R": "right",
+        "L": "left",
+        "F": "front",
+        "B": "back"
+      }[move[0]]
+
+      let times = {
+        undefined: 1,
+        "2": 2,
+        "'": 3,
+        "3": 3
+      }[move[1]]
+
+      this.rotate(side, times)
+
+    }
+  }
+
+
+  get perm(){
+    return(this.edges.map(x=>x[0].toString(16)).join("")+this.corners.map(x=>x[0]).join("")+this.edgeor()+this.corneror())
+  }
+  edgeor(){
+    return(parseInt(this.edges.map(x=>x[1]).join(""),2).toString(20).padStart(3,"0"))
+  }
+  corneror(){
+    return(parseInt(this.corners.map(x=>x[1]).join(""),3).toString(20).padStart(3,"0"))
+  }
+  resetFrom(str){
+    let edgep = str.slice(0,12).split("").map(x=>parseInt(x,20))
+    let cornerp = str.slice(12,8).split("").map(x=>parseInt(x,20))
+    console.log(edgep)
+    console.log(cornerp)
+  }
+}
+
+init()
+animate()
+function init(){
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(90, window.innerWidth / (window.innerHeight), 0.1, 1000);
+
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize(window.innerWidth, (window.innerHeight));
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	document.getElementById("webgl").appendChild(renderer.domElement);
+
+	material = new THREE.MeshBasicMaterial({color:0xffff00});
+	material = new THREE.MeshLambertMaterial({color:0xffff00});
+
+	camera.position.z = 5;
+	
+	ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+	directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+	directionalLight.position.set(1,2,0.5)
+	
+	pointLight = new THREE.DirectionalLight(0xffffff, 0.5, 1, 100);
+	
+  scene.add(ambientLight);
+  scene.add(directionalLight);
+  scene.add(pointLight);
+	
+	controls = new THREE.OrbitControls(camera, renderer.domElement)
+	controls.rotateSpeed = 0.5
+
+  theCube = new Cube()
+
+  resetScene()
+
+}
+
+function onWindowResize(){
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+}
+function keyUp(event){
+	let key = event.which || event.keyCode
+	keys[key] = false
+}
+plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+
+function animate(){
+  let delta = clock.getDelta()
+  requestAnimationFrame(animate); 
+
+  controls.update()
+  renderer.render(scene, camera);
+}
+function resetScene(){
+  //scene = new THREE.Scene();
+  scene.remove.apply(scene, scene.children);
+  scene.add(ambientLight);
+  scene.add(directionalLight);
+  scene.add(pointLight);
+  renderCube(theCube)
+}
+
+function animateRotation(side = "right", target = 0, time = 10){
+  tempfunc = (times) => {
+    if(times > 0){
+      rotation[side].angle += target / time
+      resetScene()
+      setTimeout(tempfunc, clock.getDelta(), times-1)
+    }else{
+      rotation[side].angle = 0
+      resetScene()
+    }
+  }
+  resetAngles()
+  rotation[side].angle = -target
+  tempfunc(time)
+}
+function resetAngles(){
+  for(let side in rotation){
+    rotation[side].angle = 0
+  }
+  resetScene()
+}
+
+function keyDown(event){
+  const key = event.key
+  switch(key){
+    case "e":
+      theCube.rotate("left", 3)
+      break
+    case "d":
+      theCube.rotate("left", 1)
+      break
+    case "i":
+      theCube.rotate("right", 1)
+      break
+    case "k":
+      theCube.rotate("right", 3)
+      break
+    case "f":
+      theCube.rotate("up", 3)
+      break
+    case "j":
+      theCube.rotate("up", 1)
+      break
+    case "g":
+      theCube.rotate("front", 3)
+      break
+    case "h":
+      theCube.rotate("front", 1)
+      break
+    case "s":
+      theCube.rotate("down", 1)
+      break
+    case "l":
+      theCube.rotate("down", 3)
+      break
+    case "w":
+      theCube.rotate("back", 1)
+      break
+    case "o":
+      theCube.rotate("back", 3)
+      break
+  }
+}
+
+window.addEventListener("resize", onWindowResize, false)
+//document.addEventListener('mousemove', mouseMoved, false);
+//document.addEventListener("keydown", keyDown, false);
+document.onkeydown = keyDown;
+//document.onkeyup = keyUp;
---
+++ misc.js
@@ -0,0 +1,187 @@
+// @es-nocheck
+function stateFromPerm(perm){
+
+}
+
+function getState(cube){
+    let state = Array(9).fill().map(_=>Array(12).fill(" "))
+    let set = (side, x, y, color) => {
+
+        let [xoff, yoff] = [[3, 0], [3, 3], [6, 3], [9, 3], [0, 3], [3, 6]][side]
+        state[yoff + y][xoff + x] = color
+    }
+
+
+    
+    let colors = "W G R B O Y".split(" ")
+    
+    let edges = cube.edges
+    let corners = cube.corners
+    
+    let edgeColors = "WB WR WG WO GR GO BR BO YG YR YB YO".split(" ")
+    let cornerColors = "WOB WBR WRG WGO YOG YGR YRB YBO".split(" ")    
+
+    let getEdgeColor = (ind, rot) => edgeColors[edges[ind][0]][(edges[ind][1] + rot) % 2]
+
+    let getCornerColor = (ind, rot) => cornerColors[corners[ind][0]][(3 + rot - corners[ind][1]) % 3]
+
+
+    set(0, 1, 1, "W")
+    set(1, 1, 1, "G")
+    set(2, 1, 1, "R")
+    set(3, 1, 1, "B")
+    set(4, 1, 1, "O")
+    set(5, 1, 1, "Y")
+
+
+    set(0, 1, 0, getEdgeColor(0, 0))
+    set(0, 2, 1, getEdgeColor(1, 0))
+    set(0, 1, 2, getEdgeColor(2, 0))
+    set(0, 0, 1, getEdgeColor(3, 0))
+
+    set(1, 2, 1, getEdgeColor(4, 0))
+    set(1, 0, 1, getEdgeColor(5, 0))
+    set(3, 0, 1, getEdgeColor(6, 0))
+    set(3, 2, 1, getEdgeColor(7, 0))
+
+    set(5, 1, 0, getEdgeColor(8, 0))
+    set(5, 2, 1, getEdgeColor(9, 0))
+    set(5, 1, 2, getEdgeColor(10, 0))
+    set(5, 0, 1, getEdgeColor(11, 0))
+
+
+    set(3, 1, 0, getEdgeColor(0, 1))
+    set(2, 1, 0, getEdgeColor(1, 1))
+    set(1, 1, 0, getEdgeColor(2, 1))
+    set(4, 1, 0, getEdgeColor(3, 1))
+
+    set(2, 0, 1, getEdgeColor(4, 1))
+    set(4, 2, 1, getEdgeColor(5, 1))
+    set(2, 2, 1, getEdgeColor(6, 1))
+    set(4, 0, 1, getEdgeColor(7, 1))
+
+    set(1, 1, 2, getEdgeColor(8, 1))
+    set(2, 1, 2, getEdgeColor(9, 1))
+    set(3, 1, 2, getEdgeColor(10, 1))
+    set(4, 1, 2, getEdgeColor(11, 1))
+
+
+    set(0, 0, 0, getCornerColor(0, 0))
+    set(0, 2, 0, getCornerColor(1, 0))
+    set(0, 2, 2, getCornerColor(2, 0))
+    set(0, 0, 2, getCornerColor(3, 0))
+    
+    set(5, 0, 0, getCornerColor(4, 0))
+    set(5, 2, 0, getCornerColor(5, 0))
+    set(5, 2, 2, getCornerColor(6, 0))
+    set(5, 0, 2, getCornerColor(7, 0))
+
+    set(1, 0, 0, getCornerColor(3, 1))
+    set(1, 2, 0, getCornerColor(2, 2))
+    set(1, 2, 2, getCornerColor(5, 1))
+    set(1, 0, 2, getCornerColor(4, 2))
+
+    set(3, 0, 0, getCornerColor(1, 1))
+    set(3, 2, 0, getCornerColor(0, 2))
+    set(3, 2, 2, getCornerColor(7, 1))
+    set(3, 0, 2, getCornerColor(6, 2))
+
+    set(2, 0, 0, getCornerColor(2, 1))
+    set(2, 2, 0, getCornerColor(1, 2))
+    set(2, 2, 2, getCornerColor(6, 1))
+    set(2, 0, 2, getCornerColor(5, 2))
+
+    set(4, 0, 0, getCornerColor(0, 1))
+    set(4, 2, 0, getCornerColor(3, 2))
+    set(4, 2, 2, getCornerColor(4, 1))
+    set(4, 0, 2, getCornerColor(7, 2))
+
+    console.log(state.map(x=>x.join("")).join("\n"))
+}
+
+
+
+getState(theCube)
+
+
+function setState(cube, state){
+    state = state.split("\n")
+    let get = (side, x, y) => {
+        let [xoff, yoff] = [[3, 0], [3, 3], [6, 3], [9, 3], [0, 3], [3, 6]][side]
+        return state[yoff + y][xoff + x]
+    }
+
+
+    
+    let colors = "W G R B O Y".split(" ")
+
+    
+    let edgeColors = "WB WR WG WO GR GO BR BO YG YR YB YO".split(" ")
+    let cornerColors = "WOB WBR WRG WGO YOG YGR YRB YBO".split(" ")    
+
+    let getEdgeColor = (ind, rot) => edgeColors[edges[ind][0]][(edges[ind][1] + rot) % 2]
+
+    let getCornerColor = (ind, rot) => cornerColors[corners[ind][0]][(3 + rot - corners[ind][1]) % 3]
+
+    let edges = [
+        get(0, 1, 0) + get(3, 1, 0),
+        get(0, 2, 1) + get(2, 1, 0),
+        get(0, 1, 2) + get(1, 1, 0),
+        get(0, 0, 1) + get(4, 1, 0),
+
+        get(1, 2, 1) + get(2, 0, 1),
+        get(1, 2, 1) + get(4, 2, 1),
+        get(3, 0, 1) + get(2, 2, 1),
+        get(3, 2, 1) + get(4, 0, 1),
+
+        get(5, 1, 0) + get(1, 1, 2),
+        get(5, 2, 1) + get(2, 1, 2),
+        get(5, 1, 2) + get(3, 1, 2),
+        get(5, 0, 1) + get(4, 1, 2),
+    ]
+    "WOB WBR WRG WGO YOG YGR YRB YBO"
+    let corners = [
+        get(0, 0, 0) + get(4, 0, 0) + get(3, 2, 0),
+        get(0, 2, 0) + get(3, 0, 0) + get(2, 2, 0),
+        get(0, 2, 2) + get(2, 0, 0) + get(1, 2, 0),
+        get(0, 0, 2) + get(1, 0, 0) + get(4, 2, 0),
+
+        get(5, 2, 1) + get(4, 2, 2) + get(1, 1, 0),
+        get(5, 2, 1) + get(1, 2, 2) + get(2, 1, 0),
+        get(5, 0, 1) + get(2, 2, 2) + get(3, 1, 0),
+        get(5, 2, 1) + get(3, 2, 2) + get(4, 1, 0),
+    ]
+    edges.forEach((edge, i) => {
+        let index = edgeColors.indexOf(edge)
+        if(index != -1){
+            theCube.edges[i] = [index, 0]
+        }else{
+            index = edgeColors.indexOf(edge[1] + edge[0])
+            if(index != -1){
+                theCube.edges[i] = [index, 1]
+            }
+        }
+    })
+
+    corners.forEach((corner, i) => {
+        let rotation = 0
+        let index = cornerColors.indexOf(corner)
+        if(index == -1){
+            index = cornerColors.indexOf(corner[1] + corners[2] + corners[0])
+            rotation = 1
+        }
+        if(index == -1){
+            index = cornerColors.indexOf(corner[2] + corners[0] + corners[1])
+            rotation = 2
+        }
+        if(index == -1){
+            console.error("invalid state but anyways")
+            return
+        }
+        theCube.corners[i] = [index, rotation]
+    })
+    console.log(edges)
+    console.log(get(0, 0, 0))
+
+    resetScene()
+}
